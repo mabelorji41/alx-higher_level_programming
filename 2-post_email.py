7311c092ece0614617dfba4621383ab89a4633d4
mabelorji41 <mabelorji74@gmail.com>
2024-03-28 11:57:17 -0700

    python network

---
+++ 2-post_email.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+POST request to the passed URL with the email as a parameter
+
+"""
+import urllib.request
+from sys import argv
+
+
+def main(argv):
+
+    """
+    Sends a POST request to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)
+    
+    """
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf8')
+    url = argv[1]
+    req = urllib.request.Request(url, data)
+
+    try:
+
+        with urllib.request.urlopen(req) as response:
+            result = response.read()
+            print(result.decode('utf8'))
+    except urllib.error.URLError as e:
+        print("Error:", e.reason)
+        exit(1)
+
+if __name__ == "__main__":
+    main(argv)
